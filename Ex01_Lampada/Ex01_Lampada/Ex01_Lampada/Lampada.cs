0520399af552b87cc7c2f467bfd3f7056da392f2
Liynaa <seuemail@gmail.com>
2026-04-20 22:43:06 -0400

    Ex01 Lampada concluido

---
+++ Ex01_Lampada/Ex01_Lampada/Ex01_Lampada/Lampada.cs
@@ -0,0 +1,33 @@
+﻿public class Lampada
+{
+    private string marca;
+    private readonly string tecnologia;
+    private bool ligada;
+    private int brilho;
+
+    public Lampada(string marca, string tecnologia)
+    {
+        this.marca = marca;
+        this.tecnologia = tecnologia;
+        this.ligada = false;
+        this.brilho = 100;
+    }
+
+    public void Alternar()
+    {
+        ligada = !ligada;
+    }
+
+    public void AjustarBrilho(int novoBrilho)
+    {
+        if (ligada && novoBrilho >= 0 && novoBrilho <= 100)
+        {
+            brilho = novoBrilho;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Marca: {marca} | Tecnologia: {tecnologia} | Ligada: {ligada} | Brilho: {brilho}";
+    }
+}
---
+++ Ex01_Lampada/Ex01_Lampada/Ex01_Lampada/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Lampada lampada = new Lampada("Philips", "LED");
+
+        lampada.Alternar();
+        lampada.AjustarBrilho(80);
+
+        Console.WriteLine(lampada.ToString());
+    }
+}
